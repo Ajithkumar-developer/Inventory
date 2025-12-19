280b6f82764747a85d746096abfc9336e6ebe946
hicorelearningapp <hicorelearningapp@gmail.com>
2025-12-19 12:55:08 +0530

    CORS Polisy updated

--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .api.user_api import UserAPI
 from .api.device_api import DeviceAPI
 from .api.order_api import OrderAPI
 from .api.inventory_api import InventoryAPI
 
-
-
-
-
 app = FastAPI(title="User & Auth API")
+
+# CORS configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins (DEV ONLY)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 user_api = UserAPI()
 device_api = DeviceAPI()
 order_api = OrderAPI()
 inventory_api = InventoryAPI()
-
-
 
 app.include_router(user_api.router, tags=["User"])
 app.include_router(device_api.router, tags=["Device"])
